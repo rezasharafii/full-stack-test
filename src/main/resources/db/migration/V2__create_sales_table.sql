77d8c977573c096fa44e4e21218f0f143b1cb74b
rezasharafii <shrafipoureza@gmail.com>
2025-12-30 10:23:11 +0400

    chore: add sales table

---
+++ src/main/resources/db/migration/V2__create_sales_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE sales
+(
+    id             BIGSERIAL PRIMARY KEY,
+    product_id     BIGINT         NOT NULL,
+    variant_id     BIGINT         NOT NULL,
+    quantity       INT            NOT NULL DEFAULT 1,
+    unit_price     DECIMAL(19, 2) NOT NULL,
+    tax_rate       DECIMAL(5, 2)  NOT NULL DEFAULT 0.15,
+    tax_amount     DECIMAL(19, 2) NOT NULL,
+    total_amount   DECIMAL(19, 2) NOT NULL,
+    payment_method VARCHAR(50)             DEFAULT 'CARD',
+    sold_at        TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_sales_sold_at ON sales (sold_at);
